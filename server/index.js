a4a07179caa47aeaa3ce185b2b1a3c43f53dec2c
Rahul Rana <3rahul4@gmail.com>
2018-01-19 01:05:08 +0530

    hot reloading works now

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 import express from 'express';
 import path from 'path';
-const app = express();
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev.js';
+
+let app = express();
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler,{
+  hot: true,
+  publicPath : webpackConfig.output.publicPath,
+  noInfo: true
+}));
+
+app.use(webpackHotMiddleware(compiler));
 
 app.get('/*', (request, response) => {
-  response.sendFile(path.join(__dirname,'../public/index.html'));
+  response.sendFile(path.join(__dirname,'./index.html'));
 });
 
 
